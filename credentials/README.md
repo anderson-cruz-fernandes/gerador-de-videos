b9748752865398473e1cb812deb2b94a9856879b
Anderson Cruz Fernandes <envieparaanderson@gmail.com>
2020-05-26 23:56:50 -0300

    feat: add Watson natural language understanding service

--- credentials/README.md
+++ credentials/README.md
@@ -5,6 +5,20 @@
 File: `algorithmia.json`
 ```
 {
-    "apiKey": "<KEY_OF_ALGORITHMIA"
+    "apiKey": "<KEY_OF_ALGORITHMIA>"
 }
 ```
+
+## Watson Natural Language understanding
+
+File: `watson-nlu.json`
+...
+{
+  "apikey": "<CHAVE>",
+  "iam_apikey_description": "Auto-generated for key fa054d8f-d35d-4e86-ac0c-93f544d69080",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/d2cc04b6957e4470aeed9a4e51f1d541::serviceid:ServiceId-25ffe669-36fb-4ec7-8c32-63dc00e669b3",
+  "url": "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/17d8997c-702e-407b-bdce-e7c89fed5c46"
+}
+...
--- robots/text.js
+++ robots/text.js
@@ -2,17 +2,32 @@ const algorithmia               = require("algorithmia")
 const algorithmiaApiKey         = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1')
+const { IamAuthenticator } = require('ibm-watson/auth')
+
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2019-07-12',
+  authenticator: new IamAuthenticator({
+    apikey: watsonApiKey,
+  }),
+  url: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/17d8997c-702e-407b-bdce-e7c89fed5c46',
+})
+
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 }
 
 async function fetchContentFromWikipedia(content) {
     const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
     const wikipediaAlgorithm       = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2?timeout=300')
-    const wikipediaResponde        = await wikipediaAlgorithm.pipe(content.searchTerm)    
-    const wikipediaContent         = wikipediaResponde.get()
+    const wikipediaResponse        = await wikipediaAlgorithm.pipe(content.searchTerm)    
+    const wikipediaContent         = wikipediaResponse.get()
     
     content.sourceContentOriginal = wikipediaContent.content
 }
@@ -54,4 +69,34 @@ function breakContentIntoSentences(content) {
     })
 }
 
+function LimitMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+}
+
+async function fetchKeywordsOfAllSentences(content) {
+    for (const setence of content.sentences) {
+        sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+}
+
+async function fetchWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+        naturalLanguageUnderstanding.analyze({
+            text: sentence,
+            features: {
+                keywords: {}
+            }
+        }, (error, response) => {
+            if (error) {
+                throw error
+            }
+            
+            const keywords = response.keywords.map((keyword) => { 
+                return keyword.text
+            })
+
+        resolve(keywords)
+    })
+})
+}
 module.exports = robot  
